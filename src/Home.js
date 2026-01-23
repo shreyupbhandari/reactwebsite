400df4d0f3ed59a795897327439cf2a1438a1ca1
jmint8 <jmitchelmeier@gmail.com>
2026-01-22 21:29:02 -0500

    added a dic container for the site links and images

--- src/Home.js
+++ src/Home.js
@@ -11,9 +11,11 @@ return(
 <>
 
 <header><h1>Boyle County Sites</h1></header>
+<div className="sites-container">
 {interativeData.map(sites=>
 <div className={sites.Site}>
 <Link to={`/sites/${sites.SiteID}`}><h1>{sites.Site}</h1></Link><img src ={sites.Image}/></div>)}
+</div>
 </>
 );
 
