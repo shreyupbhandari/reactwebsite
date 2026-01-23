f81cc03bb2957a998aa2312f4f41a039f5c4e00f
jmint8 <jmitchelmeier@gmail.com>
2026-01-22 22:36:58 -0500

    current version of css

--- src/SiteDetails.js
+++ src/SiteDetails.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useParams,Link} from 'react-router-dom';
-import './sites.css'; 
+import './sites.css';
 
 export default function SiteDetails({data})
 
@@ -13,9 +13,10 @@ export default function SiteDetails({data})
 
     {
         site ? <>
+        <div className = "site-level-container">
         <h2>{site.Site}</h2>
         <img src={`/${site.Image}`} alt={site.Site}/>
-        <p>{site.Description}</p> <Link to ="/">Go to Home</Link></>:<><h2>Error 404 Page Not Found!</h2>
+        <p>{site.Description}</p><Link to ="/">Go to Home</Link></div></>:<><h2>Error 404 Page Not Found!</h2>
         <Link to ="/">Go to Home</Link></>
     }
     </>
