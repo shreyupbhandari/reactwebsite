995c8ece0588a9df914a52963de846bbfd5dd5a4
Shreyup Bhandari <shreyupbhandari@gmail.com>
2026-01-26 22:33:13 -0500

    Trying to fix some of the containers in the SiteDetails page.

--- src/SiteDetails.js
+++ src/SiteDetails.js
@@ -26,18 +26,26 @@ export default function SiteDetails({ data }) {
       {site ? (
         <>
           <div className="site-level-container">
+            {/*Breadcrumb is a cool feature in webpages that help you show the page's heirarchy.*/}
+            <div className="breadcrumb">
+              <Link to="/">Home</Link>
+              <span>/</span>
+              <span>{site.Site}</span>
+            </div>
             
             <h2>{site.Site}</h2>
-            <img src={`/${site.Image}`} alt={site.Site} />
-            <p>{site.Description}</p>
+            <div className="image-container">
+            <img src={`/${site.Image}`} alt={site.Site} className="site-image"/>
+            <p className="image-short-info">{site.Site}</p>
+            </div>
+            <div className="image-description-container">{site.Description}</p></div>
             
-            <div className = "actions">
-              <div className = "buttons">
-                <div className="previousButton">
-                  {siteIndex>0&&<button onClick={previousButton}>Previousd page</button>}
+            <div className = "action-buttons">
+              <div className = "navigation-buttons">
+                  {siteIndex>0&&(<button onClick={previousButton}>←Previous Site</button>}
                 </div>
                 <div className="nextButton">
-                  {siteIndex<data.length-1&&<button onClick={nextButton}>Next Page</button>}
+                  {siteIndex<data.length-1&&<button onClick={nextButton}>Next Site→</button>}
                 </div>
               </div>
               <Link to="/">Go to Home</Link>
