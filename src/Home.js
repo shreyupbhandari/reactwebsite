7d1b5745da9e6c0e7f167e1c65a21ab019f7745e
Shreyup Bhandari <shreyupbhandari@gmail.com>
2026-01-26 19:55:15 -0500

    Putting some interactive react components like favorite categories, searchCategory by some filtering options.

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,46 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import './Home.css';
 
 export default function Home({data})
 {
 const [searchText,setSearchText]=useState('');
+const [selectedCategory, setSelectedCategory]=useState('All');
+const [sortOption,setSortOption]=useState('name-ascending');
+const [favorites,setFavorites]= useState([]);
+const [showFavoritesOnly,setShowFavoritesOnly]=useState(false);
 let interactiveData=[...data];
 
+//Using local storage to set the favorites 
+useEffect(()=>
+localStorage.setItem('boyleCountySitesFavorites',JSON.stringify(favorites))
+,[])
+
+//Retrieve those favorites set in the local storage
+useEffect(()=>
+{
+  const savedFavorites=localStorage.getItem('boyleCountySitesFavorites');
+
+  if (savedFavorites)
+{
+  setFavorites(JSON.parse(savedFavorites));
+}
+},[favorites])
+
+//This was an attempt to categorize the sites based on the site name. If the JSON file is updated with site categories, this can be a great addition.
+const getCategory=(siteName)=>
+{
+  const name=siteName.toLowerCase();
+  if(name.includes("church"))
+  {
+    return "Churches";
+  }
+  if (name.includes("house") || name.includes("inn") || name.includes())
 
 
+
+}
 if (searchText.trim()!=='')
 {
     interactiveData=interactiveData.filter(sites=>sites.Site.toLowerCase().includes(searchText.toLowerCase()));
